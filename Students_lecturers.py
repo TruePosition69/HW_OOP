ef4a890de3e8eacf750bb8b0508bb4ac568c71e5
Ilya Kupriyanov <Kynpuyanovil@gmail.com>
2024-02-25 22:27:59 +0300

    Add rates (task 2)

--- Students_lecturers.py
+++ Students_lecturers.py
@@ -6,7 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_lecturer(self, student, mark, course, lecturer):
+        if isinstance(lecturer,Lecturer) and isinstance(student, Student) and course in student.courses_in_progress and course in lecturer.courses_attached and (1 <= mark <= 10):
+            if course in lecturer.marks:
+                lecturer.marks[course] += [mark]
+            else:
+                lecturer.marks[course] = [mark]
+        else:
+            return 'Ошибка'
+                
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -18,3 +27,11 @@ class Lecturer(Mentor):
         self.marks = {}
 
 class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress and (1 <= grade <= 10):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
