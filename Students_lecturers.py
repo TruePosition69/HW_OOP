d56a836b63d02c62eb32e0616a9fffe628710832
Ilya Kupriyanov <Kynpuyanovil@gmail.com>
2024-03-05 23:44:32 +0300

    add description of students and others

--- Students_lecturers.py
+++ Students_lecturers.py
@@ -14,6 +14,23 @@ class Student:
                 lecturer.marks[course] = [mark]
         else:
             return 'Ошибка'
+        
+    def average(self):
+        self.grades_list = list(self.grades.values())
+        self.sum_grades = sum(self.grades_list)
+        self.grades_avg = self.sum_grades/len(self.grades_list)
+
+    def courses_in_prgrs(self):
+        self.delimiter = ','
+        self.courses_in_progress = self.delimiter.join(self.courses_in_progress)
+        self.finished_courses = self.delimiter.join(self.finished_courses)
+
+    def __str__(self) -> str:
+        return f'Имя: {self.name}\
+               \nФамилия: {self.surname}\
+               \nСредняя оценка за домашние задания: {round(self.grades,1)}\
+               \nКурсы в процессе изучения: {self.courses_in_progress}\
+               \nЗавершенные курсы: {self.finished_courses}'
     
 class Mentor:
     def __init__(self, name, surname):
@@ -28,14 +45,13 @@ class Lecturer(Mentor):
     def __init__(self):
         self.marks = {}
 
-    def average(self)
+    def average(self):
         self.marks_list = list(self.marks.values())
         self.sum_marks = sum(self.marks_list)
         self.marks_avg = self.sum_marks/len(self.marks_list)
 
-
     def __str__(self) -> str:
-        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции'
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {round(self.marks_avg,1)}'
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
