2150f797a2eea41cf07f1a45d44ed1ac77ee9fb0
Ilya Kupriyanov <Kynpuyanovil@gmail.com>
2024-03-06 00:17:07 +0300

    add resulsts

--- Students_lecturers.py
+++ Students_lecturers.py
@@ -16,20 +16,24 @@ class Student:
             return 'Ошибка'
         
     def average(self):
-        self.grades_list = list(self.grades.values())
-        self.sum_grades = sum(self.grades_list)
-        self.grades_avg = self.sum_grades/len(self.grades_list)
+        grades_list = list(self.grades.values())
+        self.grades_avg = sum(grades_list) / len(grades_list)
 
     def courses_in_prgrs(self):
-        self.delimiter = ','
-        self.courses_in_progress = self.delimiter.join(self.courses_in_progress)
-        self.finished_courses = self.delimiter.join(self.finished_courses)
+        delimiter = ','
+        courses_in_progress_str = delimiter.join(self.courses_in_progress)
+        return courses_in_progress_str
+    
+    def finished_courses(self):
+        delimiter = ','
+        finished_courses_str = delimiter.join(self.finished_courses)
+        return finished_courses_str
 
     def __str__(self) -> str:
         return f'Имя: {self.name}\
                \nФамилия: {self.surname}\
-               \nСредняя оценка за домашние задания: {round(self.grades,1)}\
-               \nКурсы в процессе изучения: {self.courses_in_progress}\
+               \nСредняя оценка за домашние задания: {round(self.grades_avg,1)}\
+               \nКурсы в процессе изучения: {self.courses_in_prgrs}\
                \nЗавершенные курсы: {self.finished_courses}'
     
 class Mentor:
@@ -39,7 +43,8 @@ class Mentor:
         self.courses_attached = []
 
     def __str__(self) -> str:
-        return f'Имя: {self.name} \nФамилия: {self.surname}'
+        return f'Имя: {self.name}\
+               \nФамилия: {self.surname}'
 
 class Lecturer(Mentor):
     def __init__(self):
@@ -51,7 +56,9 @@ class Lecturer(Mentor):
         self.marks_avg = self.sum_marks/len(self.marks_list)
 
     def __str__(self) -> str:
-        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {round(self.marks_avg,1)}'
+        return f'Имя: {self.name}\
+               \nФамилия: {self.surname}\
+               \nСредняя оценка за лекции: {round(self.marks_avg,1)}'
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -64,4 +71,5 @@ class Reviewer(Mentor):
             return 'Ошибка'
     
     def __str__(self) -> str:
-        return f'Имя: {self.name} \nФамилия: {self.surname}'
+        return f'Имя: {self.name}\
+               \nФамилия: {self.surname}'
