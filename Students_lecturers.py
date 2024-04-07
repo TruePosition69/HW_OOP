46f5df21bb38ef36cfcd95014337f24fb937dc4a
Ilya Kupriyanov <Kynpuyanovil@gmail.com>
2024-04-07 21:26:57 +0300

    task 3 and 4.

--- Students_lecturers.py
+++ Students_lecturers.py
@@ -18,16 +18,74 @@ class Student:
       else:
           return 'Ошибка'
 
+  def average(self):
+      all_grades = [grade for grades_list in self.grades.values() for grade in grades_list]
+      if all_grades:
+          avg_grade = sum(all_grades) / len (all_grades)
+          return avg_grade
+      else:
+          return 0
+      
+  def get_courses_in_progress(self):
+      delimiter = ', '
+      courses_in_progress_str = delimiter.join(self.courses_in_progress)
+      return courses_in_progress_str
+
+  def get_finished_courses(self):
+      return ', '.join(self.finished_courses)
+
+  def __str__(self) -> str:
+      return f'Имя: {self.name}\
+             \nФамилия: {self.surname}\
+             \nСредняя оценка за домашние задания: {self.average():.1f}\
+             \nКурсы в процессе изучения: {self.get_courses_in_progress()}\
+             \nЗавершенные курсы: {self.get_finished_courses()}'
+
+  def __gt__(self, other_student):
+      return self.average() > other_student.average()
+
+  def __lt__(self, other_student):
+      return self.average() < other_student.average()
+
+  def __eq__(self, other_student):
+      return self.average() == other_student.average()
+
 class Mentor:
   def __init__(self, name, surname):
       self.name = name
       self.surname = surname
       self.courses_attached = []
 
+  def __str__(self) -> str:
+      return f'Имя: {self.name}\
+             \nФамилия: {self.surname}'
+  
 class Lecturer(Mentor):
   def __init__(self, name, surname):
       super().__init__(name, surname)
       self.marks = {}
+
+def average(self):
+      marks_list = [mark for marks in self.marks.values() for mark in marks]
+      if marks_list:
+          self.marks_avg = sum(marks_list) / len(marks_list)
+          return self.marks_avg
+      else:
+          return 0
+
+def __str__(self) -> str:
+      return f'Имя: {self.name}\
+             \nФамилия: {self.surname}\
+             \nСредняя оценка за лекции: {round(self.marks_avg,1)}'
+  
+def __gt__(self, other_lecturers):
+      return self.average() > other_lecturers.average()
+
+def __lt__(self, other_lecturers):
+      return self.average() < other_lecturers.average()
+
+def __eq__(self, other_lecturers):
+      return self.average() == other_lecturers.average()
 
 class Reviewer(Mentor):
   def rate_hw(self, student, course, grade):
@@ -41,3 +99,45 @@ class Reviewer(Mentor):
               student.grades[course] = [grade]
       else:
           return 'Ошибка'
+      
+  def __str__(self) -> str:
+      return f'Имя: {self.name}\
+             \nФамилия: {self.surname}'
+      
+student1 = Student('Some', 'Buddy')
+student1.finished_courses = ['Введение в программирование']
+student1.courses_in_progress = ['Python', 'Git']
+student1.grades = {'Python' : [10, 9, 8, 7], 'Git' : [10, 9]}
+
+student2 = Student('Some_2', 'Buddy_2')
+student2.finished_courses = ['Введение в программирование']
+student2.courses_in_progress = ['Python', 'Git']
+student2.grades = {'Python' : [10, 9, 8, 7], 'Git' : [10, 9]}
+
+mentor1 = Mentor('Иван', 'Иванов')
+mentor1.courses_attached = ['Python', 'C##']
+mentor1.marks = {'Python': [9, 9, 10], 'C##' : [7, 8, 8]}
+
+def avg_grade_by_course(students, course_name):
+    total_grade = 0
+    num_students = 0
+    for student in students:
+        if course_name in student.courses_in_progress or course_name in student.finished_courses:
+          if course_name in student.grades:
+              total_grade += sum(student.grades[course_name])
+              num_students += len(student.grades[course_name])
+    if num_students == 0:
+        return 0
+    return total_grade / num_students
+
+def avg_marks_by_lecterer(lecturers, course_name):
+    total_grade = 0
+    num_lecturers = 0
+    for lecturer in lecturers:
+        if course_name in lecturer.courses_attached:
+            if course_name in lecturer.marks:
+                total_grade += sum(lecturer.marks[course_name])
+                num_lecturers += len(lecturer.marks[course_name])
+    if num_lecturers == 0:
+        return 0
+    return total_grade / num_lecturers
