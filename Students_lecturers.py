6fd7f4e23c62c65f96080d0c9cd9c592d5c7016f
Ilya Kupriyanov <Kynpuyanovil@gmail.com>
2024-04-07 21:17:24 +0300

    add rate_lecturers (task 2)

--- Students_lecturers.py
+++ Students_lecturers.py
@@ -6,6 +6,18 @@ class Student:
       self.courses_in_progress = []
       self.grades = {}
 
+  def rate_lecturer(self, student, mark, course, lecturer):
+      if isinstance(lecturer,Lecturer) and isinstance(student, Student) and \
+              course in student.courses_in_progress and \
+              course in lecturer.courses_attached and \
+              (1 <= mark <= 10):
+          if course in lecturer.marks:
+              lecturer.marks[course] += [mark]
+          else:
+              lecturer.marks[course] = [mark]
+      else:
+          return 'Ошибка'
+
 class Mentor:
   def __init__(self, name, surname):
       self.name = name
